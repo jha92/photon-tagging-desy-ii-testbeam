fe155bb277645589c20fa966d443fae74e39f4a6
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-08-22 20:14:02 +0200

    photon spectrum for trigger positions, todo: add second x-axis from electron calibration

---
+++ testbeam_photon_data/photon_energy/run.sh
@@ -0,0 +1,6 @@
+ python hist_mso_analyse.py 1.0_GeV_photon_180822_161149.csv 
+ python hist_mso_analyse.py 1.6_GeV_photon_180822_161719.csv 
+ python hist_mso_analyse.py 2.0_GeV_photon_180822_162135.csv 
+ python hist_mso_analyse.py 2.6_GeV_photon_180822_162545.csv 
+ python hist_mso_analyse.py 3.0_GeV_photon_180822_163315.csv 
+ python hist_mso_analyse.py 3.6_GeV_photon_180822_164004.csv 
