94d03bb894ef8aeac343109081941137d9efa00f
Jan Dreyling-Eschweiler <jan.dreyling-eschweiler@desy.de>
2018-08-22 17:01:41 +0200

    electron calibration data week two

---
+++ testbeam_electron_data/week_two_calibration/electron_calibration/pulse_height_6positions/run.sh
@@ -0,0 +1,6 @@
+ python hist_mso_analyse.py 1.0_GeV_electrons_180822_153820.csv 
+ python hist_mso_analyse.py 1.6_GeV_electrons_180822_154504.csv 
+ python hist_mso_analyse.py 2.0_GeV_electrons_180822_154852.csv 
+ python hist_mso_analyse.py 2.6_GeV_electrons_180822_155142.csv 
+ python hist_mso_analyse.py 3.0_GeV_electrons_180822_155627.csv 
+ python hist_mso_analyse.py 3.6_GeV_electrons_180822_155939.csv 
---
+++ testbeam_electron_data/week_two_calibration/electron_calibration/pulse_integral_1-6gev/run.sh
@@ -0,0 +1,8 @@
+python hist_mso_analyse.py 1.0_GeV_electrons_180821_161307.csv
+
+python hist_mso_analyse.py 1.0_GeV_electrons_180821_161307.csv
+python hist_mso_analyse.py 2.0_GeV_electrons_180821_161635.csv
+python hist_mso_analyse.py 3.0_GeV_electrons_180822_094411.csv
+python hist_mso_analyse.py 4.0_GeV_electrons_180821_161953.csv
+python hist_mso_analyse.py 5.0_GeV_electrons_180821_162346.csv
+python hist_mso_analyse.py 6.0_GeV_electrons_180821_164623.csv
